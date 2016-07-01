837859237245430505286ff5628501c96bbdc49e
Casey Kelso <casey@caseykelso.com>
2016-07-01 00:37:52 -0700

    implement stubbed macro

--- smartsheetplugin/src/main/java/com/caseykelso/confluence/SmartsheetMacro.java
+++ smartsheetplugin/src/main/java/com/caseykelso/confluence/SmartsheetMacro.java
@@ -1,33 +1,69 @@
-package com.caseykelso.confluence.SmartsheetMacro
+package com.caseykelso.confluence;
 
 import java.util.Map;
-
+import java.util.ArrayList;
+import java.util.List;
 import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
+import com.atlassian.confluence.xhtml.api.MacroDefinition;
+import com.atlassian.confluence.xhtml.api.MacroDefinitionHandler;
+import com.atlassian.confluence.xhtml.api.XhtmlContent;
+import com.atlassian.confluence.content.render.xhtml.XhtmlException;
 
-public class SmartsheetMacro implements Macro {
-	public SmartsheetMacro() {
+public class SmartsheetMacro implements Macro 
+{
+
+/*
+	public SmartsheetMacro() 
+        {
 		// TODO Auto-generated constructor stub
 	}
+*/
+
+        private final XhtmlContent xhtmlUtils;
+
+	public SmartsheetMacro(XhtmlContent xhtmlUtils) 
+	{
+		this.xhtmlUtils = xhtmlUtils;	
+	}
+
+        @Override
+        public String execute(Map<String, String> parameters, String bodyContent, ConversionContext conversionContext) throws MacroExecutionException
+        {
+                final List<MacroDefinition> macros = new ArrayList<MacroDefinition>();
+                String body = conversionContext.getEntity().getBodyAsString();
+
+		try
+		{
+		    xhtmlUtils.handleMacroDefinitions(body, conversionContext, new MacroDefinitionHandler()
+		    {
+			@Override
+			public void handle(MacroDefinition macroDefinition)
+			{
+			    macros.add(macroDefinition);
+			}
+		    });
+		}
+		catch (XhtmlException e)
+		{
+		    throw new MacroExecutionException(e);
+		}
+
+              return "SmartsheetMacro Placeholder";
+        }
+
 
 	@Override
-	public String execute(Map<String, String> arg0, String arg1,
-			ConversionContext arg2) throws MacroExecutionException {
-		// TODO Auto-generated method stub
-		return null;
+	public BodyType getBodyType() 
+        {
+                return BodyType.NONE;
 	}
 
 	@Override
-	public BodyType getBodyType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public OutputType getOutputType() {
-		// TODO Auto-generated method stub		
-		return null;
+	public OutputType getOutputType() 
+        {
+                return OutputType.BLOCK;
 	}
 }
 
