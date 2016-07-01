2bd8c6c50e7bfacd5c1e0a2d265333b29ae88186
caseykelso <casey@caseykelso.com>
2016-07-01 01:10:09 -0700

    plugin is properly loaded into confluence

--- smartsheetplugin/src/main/java/com/caseykelso/confluence/SmartsheetMacro.java
+++ smartsheetplugin/src/main/java/com/caseykelso/confluence/SmartsheetMacro.java
@@ -14,26 +14,26 @@ import com.atlassian.confluence.content.render.xhtml.XhtmlException;
 public class SmartsheetMacro implements Macro 
 {
 
-/*
+
 	public SmartsheetMacro() 
         {
 		// TODO Auto-generated constructor stub
 	}
-*/
 
-        private final XhtmlContent xhtmlUtils;
 
+//        private final XhtmlContent xhtmlUtils;
+/*
 	public SmartsheetMacro(XhtmlContent xhtmlUtils) 
 	{
 		this.xhtmlUtils = xhtmlUtils;	
 	}
-
+*/
         @Override
         public String execute(Map<String, String> parameters, String bodyContent, ConversionContext conversionContext) throws MacroExecutionException
         {
                 final List<MacroDefinition> macros = new ArrayList<MacroDefinition>();
                 String body = conversionContext.getEntity().getBodyAsString();
-
+/*
 		try
 		{
 		    xhtmlUtils.handleMacroDefinitions(body, conversionContext, new MacroDefinitionHandler()
@@ -49,8 +49,30 @@ public class SmartsheetMacro implements Macro
 		{
 		    throw new MacroExecutionException(e);
 		}
-
+*/
               return "SmartsheetMacro Placeholder";
+/*
+StringBuilder builder = new StringBuilder();
+        builder.append("<p>");
+        if (!macros.isEmpty())
+        {
+            builder.append("<table width=\"50%\">");
+            builder.append("<tr><th>Macro Name</th><th>Has Body?</th></tr>");
+            for (MacroDefinition defn : macros)
+            {
+                builder.append("<tr>");
+                builder.append("<td>").append(defn.getName()).append("</td><td>").append(defn.hasBody()).append("</td>");
+                builder.append("</tr>");
+            }
+            builder.append("</table>");
+        }
+        else
+        {
+            builder.append("You've done built yourself a macro! Nice work.");
+        }
+        builder.append("</p>");
+
+        return builder.toString(); */
         }
 
 
