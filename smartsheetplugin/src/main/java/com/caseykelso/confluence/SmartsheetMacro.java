0b504d0a71e40406781266f92b3d890eea6ddc3d
Casey Kelso <casey@caseykelso.com>
2016-07-01 00:36:05 -0700

    add ant target for launching confluence with proper jvm env

---
+++ smartsheetplugin/src/main/java/com/caseykelso/confluence/SmartsheetMacro.java
@@ -0,0 +1,33 @@
+package com.caseykelso.confluence.SmartsheetMacro
+
+import java.util.Map;
+
+import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.macro.Macro;
+import com.atlassian.confluence.macro.MacroExecutionException;
+
+public class SmartsheetMacro implements Macro {
+	public SmartsheetMacro() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String execute(Map<String, String> arg0, String arg1,
+			ConversionContext arg2) throws MacroExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BodyType getBodyType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OutputType getOutputType() {
+		// TODO Auto-generated method stub		
+		return null;
+	}
+}
+
--- smartsheetplugin/src/main/java/com/caseykelso/confluence/api/MyPluginComponent.java
+++
@@ -1,6 +0,0 @@
-package com.caseykelso.confluence.api;
-
-public interface MyPluginComponent
-{
-    String getName();
-}
