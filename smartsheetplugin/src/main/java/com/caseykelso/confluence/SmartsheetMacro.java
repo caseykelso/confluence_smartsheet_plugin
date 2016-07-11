30e22eb7293f7473cb3d41c0347274b55347b453
caseykelso <casey@caseykelso.com>
2016-07-11 01:57:44 -0700

    remove javascript cdns, conflicts with Confluence and makes the page edit button fail

--- smartsheetplugin/src/main/java/com/caseykelso/confluence/SmartsheetMacro.java
+++ smartsheetplugin/src/main/java/com/caseykelso/confluence/SmartsheetMacro.java
@@ -233,17 +233,7 @@ public class SmartsheetMacro implements Macro
        + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">"
        + "<!-- Optional theme -->"
        + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css\" integrity=\"sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r\" crossorigin=\"anonymous\">"
-       + "<!-- Latest compiled and minified JavaScript -->"
-       + "<script   src=\"https://code.jquery.com/jquery-2.2.4.min.js\"   integrity=\"sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=\"   crossorigin=\"anonymous\"></script>"
-       + "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\" integrity=\"sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS\" crossorigin=\"anonymous\">"
-       + ".level-1 > th { padding-left: 1em; }"
-       + "td { padding: 5px; }"
-       + "table { "
-       + "border-spacing: 10px;"
-       + "border-collapse: separate; "
-       + "}"
        + "</script>";
-      
        return cdns;
    }
   
