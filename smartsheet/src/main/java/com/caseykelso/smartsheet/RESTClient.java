2d7610024a80d2e03e61795b968bfa22c5ed3683
caseykelso <casey@caseykelso.com>
2016-07-28 17:00:17 -0700

    build smartsheet connector class and deploy to maven separate from cli or plug-in

--- smartsheet/src/main/java/com/caseykelso/smartsheet/RESTClient.java
+++ smartsheet/src/main/java/com/caseykelso/smartsheet/RESTClient.java
@@ -287,102 +287,11 @@ catch (Exception e)
         return doc.html();
    }
 
-	public SmartsheetMacro() 
+	public RESTClient() 
         {
 		// TODO Auto-generated constructor stub
 	}
 
-
-        @Override
-        public OutputType getOutputType()
-        {
-             return OutputType.BLOCK;
-        }
-
-	@Override
-	public BodyType getBodyType() 
-        {
-                return BodyType.NONE;
-	}
-
-        @Override
-        public String execute(Map<String, String> parameters, String bodyContent, ConversionContext conversionContext) throws MacroExecutionException
-        {
-                final List<MacroDefinition> macros = new ArrayList<MacroDefinition>();
-                String body = conversionContext.getEntity().getBodyAsString();
-
-                List<String> activeColumnNames = new ArrayList<String>();
-
-/* this isn't scalable. Switch from bools to a list of String names, this will be more flexible */
-                 if (parameters.containsKey("tags-column") && parameters.get("tags-column").equals("true"))
-                 {
-		     activeColumnNames.add("Tags");
-                 }
-
-                 if (parameters.containsKey("status-column") && parameters.get("status-column").equals("true"))
-                 {
-		     activeColumnNames.add("Status");
-                 }
-
-                 if (parameters.containsKey("task-column") && parameters.get("task-column").equals("true"))
-                 {
-		     activeColumnNames.add("Task Name");
-                 }
-
-                 if (parameters.containsKey("ryg-column") && parameters.get("ryg-column").equals("true"))
-                 {
-		     activeColumnNames.add("RYG");
-                 }
-
-                 if (parameters.containsKey("comments-column") && parameters.get("comments-column").equals("true"))
-                 {
-		     activeColumnNames.add("Comments");
-                 }
-
-                 if (parameters.containsKey("start-column") && parameters.get("start-column").equals("true"))
-                 {
-		     activeColumnNames.add("Start");
-                 }
-
-                 if (parameters.containsKey("finish-column") && parameters.get("finish-column").equals("true"))
-                 {
-		     activeColumnNames.add("Finish");
-                 }
-
-                 if (parameters.containsKey("assigned-column") && parameters.get("assigned-column").equals("true"))
-                 {
-		     activeColumnNames.add("Assigned To");
-                 }
-
-                 if (parameters.containsKey("all-column") && parameters.get("all-column").equals("true"))
-                 {
-		     activeColumnNames.add("all");
-                 }
-
-
-
-                String html = "";
-
-		try 
-		{ 
-                   String sheetID   = parameters.get("sheet-id");
-                   String tagFilter = "";
-  
-                   if (parameters.containsKey("tag-filter"))
-                   {
-                       tagFilter = parameters.get("tag-filter");
-                   }
-
-		   html = renderSheetHTML(this.getSheet(parameters.get("api-token"), Long.valueOf(sheetID)), tagFilter, activeColumnNames);
-		}
-		catch (Exception e)
-		{
-		   System.err.println("SmartSheetException: " + e.getMessage()); 
-		} 
-
-                return html;
-
-        }
 
 }
 
