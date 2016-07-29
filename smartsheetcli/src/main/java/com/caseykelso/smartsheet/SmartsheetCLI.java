c6000c2cf8c184f1619d26d193b8f97b4d90d3d1
caseykelso <casey@caseykelso.com>
2016-07-29 16:30:46 -0700

    refactor cli tool to use the standalone smartsheet connector rest client

--- smartsheetcli/src/main/java/com/caseykelso/smartsheet/SmartsheetCLI.java
+++ smartsheetcli/src/main/java/com/caseykelso/smartsheet/SmartsheetCLI.java
@@ -28,204 +28,11 @@ import org.jsoup.parser.Tag;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.caseykelso.RESTClient;
+
 
 public class SmartsheetCLI 
 {
-
-
-    public static PagedResult<Sheet> getSheets(String smartsheetToken) throws SmartsheetException
-    {
-        // Set the Access Token
-        Token token = new Token();
-        token.setAccessToken(smartsheetToken);
-
-        // Use the Smartsheet Builder to create a Smartsheet
-        Smartsheet smartsheet = new SmartsheetBuilder().setAccessToken(token.getAccessToken()).build();
-
-        // Get home with Source Inclusion parameter
-        Home home = smartsheet.homeResources().getHome(EnumSet.of(SourceInclusion.SOURCE));
-
-        // List home folders
-        List<Folder> homeFolders = home.getFolders();
-
-        for(Folder folder : homeFolders)
-        {
-            System.out.println("folder:"+folder.getName());
-        }
-
-        //List Sheets with Source Inclusion parameters and null Pagination parameters
-        PagedResult<Sheet> homeSheets = smartsheet.sheetResources().listSheets(EnumSet.of(SourceInclusion.SOURCE), null);
-
-        return homeSheets;
-
-   }
-
-   public static void renderSheets(PagedResult<Sheet> homeSheets)
-   {
-        for(Sheet sheet : homeSheets.getData())
-        {
-            System.out.println("sheet: " + sheet.getName());
-        }
-   }
-
-  
-   public static Sheet getSheet(String smartsheetToken, long sheetID) throws SmartsheetException
-   {
-        // Set the Access Token
-        Token token = new Token();
-        token.setAccessToken(smartsheetToken);
-
-        // Use the Smartsheet Builder to create a Smartsheet
-        Smartsheet smartsheet = new SmartsheetBuilder().setAccessToken(token.getAccessToken()).build();
-
-        // Get home with Source Inclusion parameter
-        Home home = smartsheet.homeResources().getHome(EnumSet.of(SourceInclusion.SOURCE));
-
-        return smartsheet.sheetResources().getSheet(sheetID, null, null, null, null, null, null, null);
-       
-   }
-
-   public static void renderSheet(Sheet s)
-   {
-        if (null != s)
-        {
-           System.out.println("sheet name: " + s.getName());
-        }
-
-   }
-
-   private static Element renderRow(Row smartsheetRow)
-   {
-      
-      Element  row                = new Element(Tag.valueOf("tr"), "");  
-      row.attr("scope", "row");
-      Element  row_number         = new Element(Tag.valueOf("th"), "");
-      row_number.appendText(smartsheetRow.getRowNumber().toString());
-
-      if (null != smartsheetRow.getParentId()) 
-      {
-        row_number.appendText(","+smartsheetRow.getParentId().toString());
-        row_number.attr("class", "level-1");
-      }
-      else
-      {
-        row_number.attr("class", "level-0");
-      }
-
-      row.appendChild(row_number);
-
-      try {
-      List<Cell> smartsheetCells  = smartsheetRow.getCells();
-
-      for (Cell smartsheetCell : smartsheetCells)
-      {
-         Element  column    = new Element(Tag.valueOf("td"), "");
-
-         if (null != smartsheetCell.getValue())
-         {
-           column.appendText(smartsheetCell.getValue().toString());
-         }
-         else
-         {
-           column.appendText("-");
-         }
-
-           row.appendChild(column);       // add column to row
-      }
-      
-      }
-      catch(Exception e)
-      {
-//          System.err.println("renderRow Exception: " + e.getMessage());
-      }
- 
-      return row; 
-   }
-
-   private static Element renderTableHeader(Sheet s)
-   {
-
-     final List<String> activeColumnNames = new ArrayList<String>() {{
-         add("RYG");
-         add("Task Name");
-         add("Start");
-         add("Finish");
-         add("Status");
-         add("Assigned To");
-//         add("Comments");
-//         add("Predecessors");
-     }};
-
-      Element tableHeader = new Element(Tag.valueOf("thead"), "");
-      Element row         = new Element(Tag.valueOf("tr"), "");
-      
-      List<Column> columns = s.getColumns();
-         
-      Element rownumberColumn = new Element(Tag.valueOf("th"), "");
-      rownumberColumn.appendText("Row Number");
-      tableHeader.appendChild(rownumberColumn);    
- 
-      for (Column c : columns)
-      {
-         if (activeColumnNames.contains(c.getTitle().toString()))
-         {
-		 Element column = new Element(Tag.valueOf("th"), "");
-		 column.appendText(c.getTitle().toString());
-		 tableHeader.appendChild(column);
-         }
-      }
-
-      tableHeader.appendChild(row);
-      return tableHeader;
-   }
-
-   private static Element renderTable(Sheet s)
-   {
-      Element  table     = new Element(Tag.valueOf("table"), ""); 
-      table.attr("class", "table");
-      table.appendChild(renderTableHeader(s));
-
-      List<Row> rows     = s.getRows(); 
-
-      for (Row r : rows)
-      {
-          table.appendChild(renderRow(r));
-      }
-
-      return table;
-   }
-
-   public static String renderCDNs()
-   {
-       String cdns = ""
-       + "<!-- Latest compiled and minified CSS -->"
-       + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">"
-       + "<!-- Optional theme -->"
-       + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css\" integrity=\"sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r\" crossorigin=\"anonymous\">"
-       + "<!-- Latest compiled and minified JavaScript -->"
-       + "<script   src=\"https://code.jquery.com/jquery-2.2.4.min.js\"   integrity=\"sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=\"   crossorigin=\"anonymous\"></script>"
-       + "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\" integrity=\"sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS\" crossorigin=\"anonymous\">"
-       + ".level-1 > th { padding-left: 1em; }"
-       + "</script>";
-      
-       return cdns;
-   }
-  
-   
-   public static String renderSheetHTML(Sheet s)
-   {
-        Document doc       = Document.createShell("");
-        
-        DataNode script    = new DataNode("script", "");
-        script.setWholeData(renderCDNs());
-        doc.head().appendChild(script);
-
-        Element  headline  = doc.body().appendElement("h1").text(s.getName());
-        
-        doc.body().appendChild(renderTable(s)); // add table to html body
-
-        return doc.html();
-   }
 
    public static Options setupCommandLine()
    {
@@ -258,6 +65,7 @@ public class SmartsheetCLI
         Properties props           = null;
         Options    options         = null;
         CommandLine cl             = null;
+        RESTClient  client         = new RESTClient();
 
         options = setupCommandLine();
         CommandLineParser clparser = new DefaultParser();
@@ -281,24 +89,26 @@ public class SmartsheetCLI
           System.exit(1);
         }        
 
-        if (cl.hasOption("allsheets"))
+/*        if (cl.hasOption("allsheets"))
         {   
 		try 
 		{ 
-		   renderSheets(SmartsheetCLI.getSheets(props.getProperty("apitoken")));
+		   client.renderSheets(client.getSheets(props.getProperty("apitoken")));
 		}
 		catch (Exception e)
 		{
 		   System.err.println("SmartSheetException: " + e.getMessage()); 
 		   System.exit(2);
 		}
-        } 
-        else if (cl.hasOption("sheet") && (null != cl.getOptionValue("sheet")))
+        }  */
+        if (cl.hasOption("sheet") && (null != cl.getOptionValue("sheet")))
         {   
 		try 
 		{ 
                    String html = "";
-		   html = renderSheetHTML(SmartsheetCLI.getSheet(props.getProperty("apitoken"), Long.valueOf(cl.getOptionValue("sheet"))));
+                   List<String> columns = new ArrayList<String>();
+                   columns.add("all");
+		   html = client.renderSheetHTML(client.getSheet(props.getProperty("apitoken"), Long.valueOf(cl.getOptionValue("sheet"))),"",columns);
                    System.out.println(html);
 		}
 		catch (Exception e)
