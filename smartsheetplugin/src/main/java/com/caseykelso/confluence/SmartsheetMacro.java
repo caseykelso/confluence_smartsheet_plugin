70e77150cb1cb4ee48c046d9e0d56b7e0872fcad
caseykelso <casey@caseykelso.com>
2016-08-03 17:55:27 -0700

    debugging funky behavior with hide columns

--- smartsheetplugin/src/main/java/com/caseykelso/confluence/SmartsheetMacro.java
+++ smartsheetplugin/src/main/java/com/caseykelso/confluence/SmartsheetMacro.java
@@ -43,13 +43,11 @@ public class SmartsheetMacro implements Macro
 {
 
 	private List<Integer> activeColumnIndexes; //TODO: refactor, use a class instance per macro execute
-        private int           tagsColumn; //TODO: same as above
         private com.caseykelso.RESTClient client;
-        private boolean       isHideColumnHeaders;
 
 	public SmartsheetMacro() 
         {
-            client = new RESTClient();
+
 	}
 
 
@@ -68,68 +66,81 @@ public class SmartsheetMacro implements Macro
         @Override
         public String execute(Map<String, String> parameters, String bodyContent, ConversionContext conversionContext) throws MacroExecutionException
         {
-                final List<MacroDefinition> macros = new ArrayList<MacroDefinition>();
-                String body = conversionContext.getEntity().getBodyAsString();
+                final List<MacroDefinition> macros              = new ArrayList<MacroDefinition>();
+                String                      body                = conversionContext.getEntity().getBodyAsString();
+                List<String>                activeColumnNames   = new ArrayList<String>();
+                boolean                     isHideColumnHeaders = false;
+                String                      html                = "";
+                client                                          = new RESTClient();
 
-                List<String> activeColumnNames = new ArrayList<String>();
 
 /* this isn't scalable. Switch from bools to a list of String names, this will be more flexible */
                  if (parameters.containsKey("tags-column") && parameters.get("tags-column").equals("true"))
                  {
 		     activeColumnNames.add("Tags");
+                     html += ("************tags-column");
                  }
 
                  if (parameters.containsKey("status-column") && parameters.get("status-column").equals("true"))
                  {
 		     activeColumnNames.add("Status");
+                     html += ("************status-column");
+ 
                  }
 
                  if (parameters.containsKey("task-column") && parameters.get("task-column").equals("true"))
                  {
 		     activeColumnNames.add("Task Name");
+                     html += ("************task-column");
+ 
                  }
 
                  if (parameters.containsKey("ryg-column") && parameters.get("ryg-column").equals("true"))
                  {
 		     activeColumnNames.add("RYG");
+                     html += ("************ryg-column");
+ 
                  }
 
                  if (parameters.containsKey("comments-column") && parameters.get("comments-column").equals("true"))
                  {
 		     activeColumnNames.add("Comments");
+                     html += ("************comments-column");
+ 
                  }
 
                  if (parameters.containsKey("start-column") && parameters.get("start-column").equals("true"))
                  {
 		     activeColumnNames.add("Start");
+                     html += ("************start-column");
                  }
 
                  if (parameters.containsKey("finish-column") && parameters.get("finish-column").equals("true"))
                  {
 		     activeColumnNames.add("Finish");
+                     html += ("************finish-column");
                  }
 
                  if (parameters.containsKey("assigned-column") && parameters.get("assigned-column").equals("true"))
                  {
+                     html += ("************assigned-column");
 		     activeColumnNames.add("Assigned To");
                  }
 
                  if (parameters.containsKey("all-column") && parameters.get("all-column").equals("true"))
                  {
 		     activeColumnNames.add("all");
+                     html += ("************all-columns");
+ 
                  }
 
-                 if (parameters.containsKey("hide-column-headers") && parameters.get("hide-column-headers").equals("true"))
+
+                 if (parameters.containsKey("hide-headers") && parameters.get("hide-headers").equals("true"))
                  {
                      isHideColumnHeaders = true;
-                 }
-                 else
-                 {
-                     isHideColumnHeaders = false;
+                     html += ("************hide-headers");
                  }
 
-
-                String html = "";
 
 		try 
 		{ 
@@ -139,13 +150,15 @@ public class SmartsheetMacro implements Macro
                    if (parameters.containsKey("tag-filter"))
                    {
                        tagFilter = parameters.get("tag-filter");
+                       html += ("************tag-filter: "+ tagFilter);
                    }
 
-		   html = client.renderSheetHTML(client.getSheet(parameters.get("api-token"), Long.valueOf(sheetID)), tagFilter, activeColumnNames, isHideColumnHeaders);
+		   html += client.renderSheetHTML(client.getSheet(parameters.get("api-token"), Long.valueOf(sheetID)), tagFilter, activeColumnNames, isHideColumnHeaders);
 		}
 		catch (Exception e)
 		{
 		   System.err.println("SmartSheetException: " + e.getMessage()); 
+                   html += ("SmartSheetException: " + e.getMessage());
 		} 
 
                 return html;
